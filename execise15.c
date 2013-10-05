f220c7e68dd581366bc2d58efa0d897a03cd74ea
mars <337240552@qq.com>
2013-10-05 09:54:52 +0800

    commit deleted file

--- execise15.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-main(){
-	int fahr ;
-	for(fahr = 300; fahr >= 0; fahr --){
-		printf("%d\n", fahr);
-	}
-}
--- execise16.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char const *argv[])
-{
-	printf("%d\n", getchar() != EOF);
-	printf("%d\n", EOF);
-	return 0;
-}
